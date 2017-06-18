c0984337bb8207a293d2df5f8c80000f54eba425
solairerove <hax2033@gmail.com>
2017-06-18 21:54:07 +0300

    Initial commit

---
+++ src/main/groovy/com/github/solairerove/facepalm/spring/groovy/FacePalmSpringGroovyApplication.groovy
@@ -0,0 +1,12 @@
+package com.github.solairerove.facepalm.spring.groovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class FacePalmSpringGroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run FacePalmSpringGroovyApplication, args
+	}
+}
---
+++ src/test/groovy/com/github/solairerove/facepalm/spring/groovy/FacePalmSpringGroovyApplicationTests.groovy
@@ -0,0 +1,16 @@
+package com.github.solairerove.facepalm.spring.groovy
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner)
+@SpringBootTest
+class FacePalmSpringGroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
