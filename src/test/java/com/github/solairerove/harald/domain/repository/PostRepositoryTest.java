1708cbd11fffbadc53e075f3722ec97e1e66a109
solairerove <hax2033@gmail.com>
2017-09-11 16:16:04 +0300

    feat: add module tests for repository and post service - use embedded db for tests

---
+++ src/test/java/com/github/solairerove/harald/domain/repository/PostRepositoryTest.java
@@ -0,0 +1,106 @@
+package com.github.solairerove.harald.domain.repository;
+
+import com.github.solairerove.harald.domain.model.Post;
+import org.hamcrest.core.Is;
+import org.hamcrest.core.IsNull;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostRepositoryTest {
+
+    private static final Long NON_EXISTS_ID = 100L;
+
+    private Post post;
+
+    private Long id;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Before
+    public void setUp() {
+        postRepository.deleteAllInBatch();
+
+        post = new Post();
+        post.setTitle("Some awesome title");
+        post.setAuthor("solairerove");
+        post.setDate("03.07.1996");
+        post.setContent("My Content");
+
+        id = postRepository.save(post).getId();
+    }
+
+    @Test
+    public void findOneByIdTest_expect_success() {
+        final Post saved = postRepository.findOneById(id).orElse(null);
+
+        assertThat(saved, Is.is(post));
+    }
+
+    @Test
+    public void findOneByIdTest_withNonExistsId() {
+        final Post saved = postRepository.findOneById(NON_EXISTS_ID).orElse(null);
+
+        assertThat(saved, IsNull.nullValue(Post.class));
+    }
+
+    @Test
+    public void updateOneByIdTest_updateField_expect_success() {
+        final String updateAuthor = "Update author";
+
+        final Post saved = postRepository.findOne(id);
+        saved.setAuthor(updateAuthor);
+
+        postRepository.updateOneById(id,
+                saved.getTitle(),
+                saved.getAuthor(),
+                saved.getDate(),
+                saved.getContent());
+
+        assertThat(postRepository.findOne(id).getAuthor(), Is.is(updateAuthor));
+    }
+
+    @Test
+    public void updateOneByIdTest_withoutUpdate_expect_success() {
+        final Post saved = postRepository.findOne(id);
+
+        postRepository.updateOneById(id,
+                saved.getTitle(),
+                saved.getAuthor(),
+                saved.getDate(),
+                saved.getContent());
+
+        assertThat(postRepository.findOne(id).getAuthor(), Is.is(saved.getAuthor()));
+    }
+
+    @Test
+    public void updateOneByIdTest_withNullFields_expect_success() {
+        final Post saved = postRepository.findOne(id);
+
+        postRepository.updateOneById(id, null, null, null, null);
+
+        assertThat(postRepository.findOne(id).getAuthor(), Is.is(saved.getAuthor()));
+    }
+
+    @Test
+    public void deletePostByIdTest_expect_success() {
+        postRepository.deletePostById(id);
+
+        assertThat(0L, Is.is(postRepository.count()));
+    }
+
+    @Test
+    public void deletePostByIdTest_expect_nonDeleteState() {
+        postRepository.deletePostById(NON_EXISTS_ID);
+
+        assertThat(1L, Is.is(postRepository.count()));
+    }
+}
---
+++ src/test/java/com/github/solairerove/harald/domain/service/PostServiceTest.java
@@ -0,0 +1,104 @@
+package com.github.solairerove.harald.domain.service;
+
+import com.github.solairerove.harald.domain.model.Post;
+import com.github.solairerove.harald.domain.model.exception.ResourceNotFoundException;
+import com.github.solairerove.harald.domain.repository.PostRepository;
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostServiceTest {
+
+    private static final Long NON_EXISTS_ID = 100L;
+
+    private Post post;
+
+    private Long id;
+
+    @Autowired
+    private PostRepository postRepository;
+
+    @Autowired
+    private PostService postService;
+
+    @Before
+    public void setUp() {
+        postRepository.deleteAllInBatch();
+
+        post = new Post();
+        post.setTitle("Some awesome title");
+        post.setAuthor("solairerove");
+        post.setDate("26-04-1986");
+        post.setContent("My Content");
+
+        id = postRepository.save(post).getId();
+    }
+
+    @Test
+    public void createTest_expect_success() {
+        final String content = "One more saved post";
+        post.setContent(content);
+
+        final Post saved = postService.create(post);
+
+        assertThat(saved.getContent(), Is.is(content));
+    }
+
+    @Test
+    public void fetchByIdTest_expect_success() {
+        final Post fetched = postService.fetchById(id);
+
+        assertThat(fetched, Is.is(post));
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void fetchByIdTest_withIncorrectId_expect_ResourceNotFound() {
+        postService.fetchById(NON_EXISTS_ID);
+    }
+
+    @Test
+    public void updateByIdTest_assertUpdatedField_expect_success() {
+        final String title = "Some updated awesome title";
+
+        final Post updated = new Post();
+        updated.setTitle(title);
+
+        postService.updateById(id, updated);
+
+        assertThat(postRepository.findOne(id).getTitle(), Is.is(title));
+    }
+
+    @Test
+    public void updateByIdTest_assertNonUpdatedField_expect_success() {
+        final String title = "Some updated awesome title";
+
+        final Post updated = new Post();
+        updated.setTitle(title);
+
+        postService.updateById(id, updated);
+
+        assertThat(postRepository.findOne(id).getContent(), Is.is(post.getContent()));
+    }
+
+    @Test
+    public void deleteByIdTest_assertPost_expect_success() {
+        final Post deleted = postService.deleteById(id);
+
+        assertThat(deleted, Is.is(post));
+    }
+
+    @Test
+    public void deleteByIdTest_assertCount_expect_success() {
+        postService.deleteById(id);
+
+        assertThat(postRepository.count(), Is.is(0L));
+    }
+}
